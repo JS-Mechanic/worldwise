0a54d65df5ef31b602f6fa9b6b9db795528043d3
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-07-29 11:28:01 +0330

    Using Outlet inside Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,12 +2,13 @@ import styles from "./Sidebar.module.css";
 import Logo from "./Logo.jsx";
 import AppNav from "./AppNav.jsx";
 import Footer from "./Footer.jsx";
+import {Outlet} from "react-router-dom";
 export default function Sidebar() {
 	return (
 		<div className={styles.sidebar}>
 			<Logo />
 			<AppNav />
-			<p>List of cities</p>
+			<Outlet />
 			<Footer />
 		</div>
 	);
