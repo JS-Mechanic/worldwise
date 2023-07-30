2d75b2159e4cfffba0a1c7cca94da1fce38e4fa8
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-07-30 11:21:28 +0330

    Dynamic router with url parameters

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Login from "./pages/Login.jsx";
 import CityList from "./components/CityList.jsx";
 import {useState, useEffect} from "react";
 import CountryList from "./components/CountryList.jsx";
+import City from "./components/City.jsx";
 
 const BASE_URL = "https://my-json-server.typicode.com/JS-Mechanic/restapi";
 
@@ -41,6 +42,7 @@ function App() {
 				<Route path="app" element={<AppLayout />}>
 					<Route index element={<CityList cities={cities} isLoading={isLoading} />} />
 					<Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
+					<Route path="cities/:id" element={<City />} />
 					<Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
 					<Route path="form" element={<p>Form</p>} />
 				</Route>
