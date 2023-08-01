d6fa9bf690a409a97737c66429f8bc09535afe46
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-08-01 09:18:19 +0330

    Removing unnecessary props and using CitiesProvider

--- src/App.jsx
+++ src/App.jsx
@@ -6,50 +6,31 @@ import NotFound from "./pages/NotFound.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
 import Login from "./pages/Login.jsx";
 import CityList from "./components/CityList.jsx";
-import {useState, useEffect} from "react";
 import CountryList from "./components/CountryList.jsx";
 import City from "./components/City.jsx";
 import Form from "./components/Form.jsx";
-
-const BASE_URL = "https://my-json-server.typicode.com/JS-Mechanic/restapi";
+import {CitiesProvider} from "./contexts/CitiesContext.jsx";
 
 function App() {
-	const [cities, setCities] = useState({});
-	const [isLoading, setIsLoading] = useState(false);
-
-	useEffect(() => {
-		async function fetchCities() {
-			try {
-				setIsLoading(true);
-				const res = await fetch(`${BASE_URL}/cities`);
-				const data = await res.json();
-				setCities(data);
-			} catch (e) {
-				alert(e.message);
-			} finally {
-				setIsLoading(false);
-			}
-		}
-		fetchCities();
-	}, []);
-
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route index element={<Home />} />
-				<Route path="product" element={<Product />} />
-				<Route path="pricing" element={<Pricing />} />
-				<Route path="login" element={<Login />} />
-				<Route path="app" element={<AppLayout />}>
-					<Route index element={<Navigate replace to="cities" />} />
-					<Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
-					<Route path="cities/:id" element={<City />} />
-					<Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
-					<Route path="form" element={<Form />} />
-				</Route>
-				<Route path="*" element={<NotFound />} />
-			</Routes>
-		</BrowserRouter>
+		<CitiesProvider>
+			<BrowserRouter>
+				<Routes>
+					<Route index element={<Home />} />
+					<Route path="product" element={<Product />} />
+					<Route path="pricing" element={<Pricing />} />
+					<Route path="login" element={<Login />} />
+					<Route path="app" element={<AppLayout />}>
+						<Route index element={<Navigate replace to="cities" />} />
+						<Route path="cities" element={<CityList />} />
+						<Route path="cities/:id" element={<City />} />
+						<Route path="countries" element={<CountryList />} />
+						<Route path="form" element={<Form />} />
+					</Route>
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</BrowserRouter>
+		</CitiesProvider>
 	);
 }
 
