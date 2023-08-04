d571b9286c6109872607884d3b322e9411ca601e
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-08-04 12:45:30 +0330

    Fixing a minor issue of early return condition

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -7,7 +7,7 @@ import {useCities} from "../contexts/CitiesContext.jsx";
 export default function CityList() {
 	const {cities, isLoading} = useCities();
 	if (isLoading) return <Spinner />;
-	if (!cities.length)
+	if (cities.length === 0)
 		return <Message message="Add your first city by clicking on a city on the map" />;
 	return (
 		<ul className={styles.cityList}>
