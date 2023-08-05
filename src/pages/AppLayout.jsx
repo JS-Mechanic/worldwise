fdf12c9367e4d7eff19674f8b5fe2013b3a92633
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-08-05 14:41:32 +0330

    Importing relevant modules, using useAuth custom hook and conditional rendering

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,12 +1,17 @@
 import Sidebar from "../components/Sidebar.jsx";
 import styles from "./AppLayout.module.css";
 import Map from "../components/Map.jsx";
+import {useAuth} from "../contexts/FakeAuthContext.jsx";
+import User from "../components/User.jsx";
 
 export default function AppLayout() {
+	const {isAuthenticated} = useAuth();
+
 	return (
 		<div className={styles.app}>
 			<Sidebar />
 			<Map />
+			{isAuthenticated && <User />}
 		</div>
 	);
 }
