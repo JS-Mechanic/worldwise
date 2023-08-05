b010f3277b384d7d41c043f4c5eaa66a70864385
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-08-05 10:17:19 +0330

    Using reducer function inside deleteCity handler

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -84,16 +84,14 @@ export function CitiesProvider({children}) {
 	}
 
 	async function deleteCity(id) {
+		dispatch({type: "loading"});
 		try {
-			setIsLoading(true);
 			await fetch(`${BASE_URL}/cities/${id}`, {
 				method: "DELETE",
 			});
-			setCities(cities => cities.filter(city => city.id !== id));
+			dispatch({type: "city/deleted", payload: id});
 		} catch (e) {
-			alert("There was an error deleting city!");
-		} finally {
-			setIsLoading(false);
+			dispatch({type: "rejected", payload: "There was an error deleting city!"});
 		}
 	}
 
