52d41cc64c1eb77fc00b80efa81bfb77ced54198
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-08-05 15:28:05 +0330

    Removing redundant codes

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,17 +1,14 @@
 import Sidebar from "../components/Sidebar.jsx";
 import styles from "./AppLayout.module.css";
 import Map from "../components/Map.jsx";
-import {useAuth} from "../contexts/FakeAuthContext.jsx";
 import User from "../components/User.jsx";
 
 export default function AppLayout() {
-	const {isAuthenticated} = useAuth();
-
 	return (
 		<div className={styles.app}>
 			<Sidebar />
 			<Map />
-			{isAuthenticated && <User />}
+			<User />
 		</div>
 	);
 }
